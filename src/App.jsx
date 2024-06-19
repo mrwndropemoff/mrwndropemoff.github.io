8e80abf1048be2492822e45b978258acd9902e24
mrwn <jemhourmarwane@gmail.com>
2024-06-19 21:36:00 +0100

    readapte

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import { FaInstagram, FaWhatsapp, FaTiktok } from 'react-icons/fa';
 
 function App() {
   return (
-    <div className="flex flex-col justify-center items-center h-screen WS bg-gray-200 ">
-      <p className="text-6xl font-extrabold text-gray-900">OPIUM MYSTIQUE SCENTS</p>
+    <div className="flex flex-col justify-center items-center h-screen w-screen bg-gray-200 ">
+      <p className="text-6xl font-extrabold text-gray-900 text-center">OPIUM MYSTIQUE SCENTS</p>
       <p className="text-2xl text-gray-700 mt-4">Coming Soon!!</p>
+      <p className="text-lg text-gray-700 mt-96">Contact us </p>
       <div className="mt-8 flex space-x-4">
         <a href="https://www.instagram.com/opiumfragrance_" className="text-blue-500 hover:text-blue-700 flex items-center space-x-2">
           <FaInstagram className="w-10 h-10" />
